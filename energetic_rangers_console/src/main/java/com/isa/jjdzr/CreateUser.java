a83f33aad7d5732e5b87bd507913831da18af216
Katarzyna Kulas <kulas.kat@gmail.com>
2023-01-28 20:20:02 +0100

    Create fields and constructor in User calss

--- energetic_rangers_console/src/main/java/com/isa/jjdzr/CreateUser.java
+++ energetic_rangers_console/src/main/java/com/isa/jjdzr/CreateUser.java
@@ -1,4 +1,8 @@
 package com.isa.jjdzr;
 
 public class CreateUser {
+
+    public static void crateUser (String name, String surname, String email, String password) {
+
+    }
 }
--- energetic_rangers_console/src/main/java/com/isa/jjdzr/User.java
+++ energetic_rangers_console/src/main/java/com/isa/jjdzr/User.java
@@ -1,4 +1,16 @@
 package com.isa.jjdzr;
 
 public class User {
+    private String id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+
+    public User (String name, String surname, String email, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+    }
 }
